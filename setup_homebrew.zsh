7087c0f9c8e40b26ee6131a0de012df2fa84c038
Henry Chou <38032437+henry97113@users.noreply.github.com>
2021-05-27 19:50:34 +0800

    switch to Brewfile for Homebrew install
    various package I use on daily basis

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -1,11 +1,7 @@
 #!/usr/bin/env zsh
 
 echo "\n<<< Starting Homebrew Setup >>>\n"
+
+# Install Homebrew Packages
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-# Install packages
-brew install httpie
-brew install bat
-
-brew install --cask visual-studio-code
-brew install --cask firefox
+brew bundle --verbose
