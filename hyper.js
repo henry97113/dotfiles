96196446a71b152c0eafe5daf947cd66dae4c5e6
Henry Chou <38032437+henry97113@users.noreply.github.com>
2022-03-18 14:08:17 +0800

    update .hyper.js

--- hyper.js
+++ hyper.js
@@ -11,7 +11,7 @@ module.exports = {
     fontSize: 14,
     // font family with optional fallbacks
     fontFamily:
-      '"MesloLGS NF", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+      '"MesloLGS Nerd Font Mono", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
     // font weight for bold characters: 'normal' or 'bold'
@@ -138,7 +138,12 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyper-aura-theme', 'hyper-quit', 'hyper-active-tab'],
+  plugins: [
+    'hyper-aura-theme',
+    'hyper-quit',
+    'hyper-active-tab',
+    'hyper-tokyo-night',
+  ],
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
